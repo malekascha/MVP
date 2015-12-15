1b9d46f7d2144939f0700307b828d734864084e2
malekascha <malekascha@gmail.com>
2015-12-15 15:23:51 -0800

    map fully functional

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+
+angular
+  .module('mvpApp', [
+    'ngRoute',
+    'mvpApp.map'
+    ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'scripts/views/main.html',
+        controller: 'mapController',
+        controllerAs: 'vm'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .factory('infoDisplay', function($rootScope){
+    var displayMethods = {};
+    displayMethods.windowDisplay = function(a, marker, context){
+     context.currentMarker = marker;
+     $rootScope.map.showInfoWindow('post-data');
+     $('.tabber').click(function(e){
+      e.preventDefault();
+      $(this).tab('show');
+     });
+    }
+    return displayMethods;
+  });
