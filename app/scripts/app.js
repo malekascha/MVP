5666670c834557b14fa95b57b4fe9f4b731419b0
malekascha <malekascha@gmail.com>
2015-12-14 17:09:54 -0800

    Map displays, markers are added, markers can be clicked to display info

---
+++ app/scripts/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mvpApp
+ * @description
+ * # mvpApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mvpApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngMap',
+    'mvpApp.map'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'mapController',
+        controllerAs: 'vm'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .run(function($rootScope, NgMap){
+    NgMap.getMap().then(function(map){
+      $rootScope.map = map;
+    })
+  })
+  .factory('infoDisplay', function($rootScope){
+    var displayMethods = {};
+    displayMethods.windowDisplay = function(a, marker, context){
+     context.currentMarker = marker;
+     $rootScope.map.showInfoWindow('post-data');
+     // $('#tabs').tabs();
+    }
+    return displayMethods;
+  });
---
+++ app/scripts/controllers/map/mapController.js
@@ -0,0 +1,8 @@
+angular.module('mvpApp.map', [])
+  .controller('mapController', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
